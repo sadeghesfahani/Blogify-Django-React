80de65ef3171c3c73f7871744726af299c5903c6
Sina <31625783+sadeghesfahani@users.noreply.github.com>
2021-08-18 19:06:18 +0430

    serializer has been set, category created

--- Blogify/api/models.py
+++ Blogify/api/models.py
@@ -1,5 +1,36 @@
 from django.db import models
 
+
 # Create your models here.
 
+class Category(models.Model):
+    category_title = models.CharField(max_length=150, null=False, blank=False)
+    category_description = models.CharField(max_length=2500, blank=True, default="")
+    parent = models.ForeignKey("self", on_delete=models.PROTECT, blank=True, null=True)
+    show_in_navbar = models.BooleanField(default=True)
 
+    def __str__(self):
+        if self.parent:
+            return Category.get_sub(self)
+        else:
+            return f"{self.category_title}"
+
+    temp_list = list()
+
+    @staticmethod
+    def get_sub(obj):
+
+        while obj:
+            Category.temp_list.append(obj.category_title)
+            obj = obj.parent
+        string_to_return = str()
+        Category.temp_list.reverse()
+        counter = 0
+        for sec in Category.temp_list:
+            if counter == 0:
+                string_to_return += f"{sec}"
+            else:
+                string_to_return += f" --> {sec}"
+            counter += 1
+        Category.temp_list = list()
+        return string_to_return
--- Blogify/api/views.py
+++ Blogify/api/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Category
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status, generics
+from .serializers import CategorySerializer
 
 # Create your views here.
+
+
+class FetchCategory(generics.CreateAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
