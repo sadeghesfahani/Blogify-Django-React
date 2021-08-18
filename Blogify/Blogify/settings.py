ed5ab57a926641a14d0867272bde92e7dee4b65e
Sina <31625783+sadeghesfahani@users.noreply.github.com>
2021-08-18 12:10:20 +0430

    database connected to postgresql

--- Blogify/Blogify/settings.py
+++ Blogify/Blogify/settings.py
@@ -75,12 +75,25 @@ WSGI_APPLICATION = 'Blogify.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'blogify',
+        'USER': 'postgres',
+        'PASSWORD': 'sina335099',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
